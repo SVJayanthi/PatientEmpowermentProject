91bc8d6bf3faa8e07d2287dd37af1e90d16057b3
SVJayanthi <sravanvjayanthi@gmail.com>
2019-06-18 16:59:36 -0400

    first

---
+++ js/login.js
@@ -0,0 +1,23 @@
+// Plugin options and our code
+$("#modal_trigger").leanModal({
+		top: 100,
+		overlay: 0.6,
+		closeButton: ".modal_close"
+});
+
+$(function() {
+		// Calling Login Form
+		$("#login_form").click(function() {
+				$(".social_login").hide();
+				$(".user_login").show();
+				return false;
+		});
+
+		// Going back to Social Forms
+		$(".back_btn").click(function() {
+				$(".user_login").hide();
+				$(".social_login").show();
+				$(".header_title").text('Login');
+				return false;
+		});
+});
